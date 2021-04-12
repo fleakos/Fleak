1d867652a475d79c32faf8774ef241fa50c46983
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-12 00:48:01 -0300

    Add files via upload

---
+++ src/include/errno.h
@@ -0,0 +1,9 @@
+#ifndef _ERRNO_H
+#define _ERRNO_H
+
+#define ERRGEN 99 /* Generic Error */
+#define ERRPERM 1 /* operation not allowed */
+#define ERRNOTF 2 /* No such file or directory */
+#define ERRPROC 3 /* não existe tal processo*/
+#define EFUNCALL 4 /* chamada de função interrompida */
+#define ERRIO 5 /* Input/Output errors */
