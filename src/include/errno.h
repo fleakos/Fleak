95b2c381ded4dded9365e0f1de4b53f1fae13a5f
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-12 03:07:39 -0300

    Translating comments

--- src/include/errno.h
+++ src/include/errno.h
@@ -4,8 +4,9 @@
 #define ERRGEN 99 /* Generic Error */
 #define ERRPERM 1 /* operation not allowed */
 #define ERRNOTF 2 /* No such file or directory */
-#define ERRPROC 3 /* não existe tal processo*/
-#define EFUNCALL 4 /* chamada de função interrompida */
+#define ERRPROC 3 /* there is no such process */
+#define EFUNCALL 4 /* interrupted function call */
 #define ERRIO 5 /* Input/Output errors */
+#define ERRVICE 6 /* there is no such device or address */
 
 #endif /* _ERRNO_H */
