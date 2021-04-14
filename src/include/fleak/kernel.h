cc44803193a89f02bfb7d1b7aee0ecded6c073c1
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-13 22:06:42 -0300

    Att headder kernel.h

--- src/include/fleak/kernel.h
+++
@@ -1,5 +0,0 @@
-void verify_area(void * addr,int count);
-volatile void kernel_panic(const char * str);
-int printf(const char * fmt, ...);
-int pk(const char * fmt, ...);
-int write(unsigned ch,char * buf,int count);
