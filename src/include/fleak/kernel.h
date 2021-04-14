2116b35424f3203d932c41dbcd455c70242a16ca
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-13 22:07:15 -0300

    Removing an archaic C string

---
+++ src/include/fleak/kernel.h
@@ -0,0 +1,5 @@
+void verify_area(void * addr,int count);
+void kernel_panic(const char * str);
+int printf(const char * fmt, ...);
+int pk(const char * fmt, ...);
+int write(unsigned ch,char * buf,int count);
