92f9009a92bbf5e55683df41a3ca4c38bae76fea
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-14 01:56:34 -0300

    add macros

--- src/include/fleak/kernel.h
+++ src/include/fleak/kernel.h
@@ -1,5 +1,14 @@
+#ifndef _FLEAK_KERNEL_H
+#define _FLEAK_KERNEL_H
+
 void verify_area(void * addr,int count);
+
 void kernel_panic(const char * str);
+
 int printf(const char * fmt, ...);
+
 int pk(const char * fmt, ...);
+
 int write(unsigned ch,char * buf,int count);
+
+#endif /* _FLEAK_KERNEL_H */
