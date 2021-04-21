70d7a82b9e9d7621f52fd4f2a8881233598d06db
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-21 14:02:50 -0300

    Fixing the architecture

--- src/include/command/echo.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ECHO_H
-#define _ECHO_H
-
-#define SIZE 2048
-char buf[SIZE];
-int count;
-
-#endif /* _ECHO_H */
--- src/kernel/kernel_panic.c
+++
@@ -1,7 +0,0 @@
-#include "/../fleak/kernel.h"
-
-volatile void kernel_panic(const char * s){
-    pk("Kernel panic: %s\n\n\r", s);
-    for(;;);
-}
-
