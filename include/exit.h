f09e6f41a54c5ce665e6928aea523ec85858118a
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-26 00:44:30 -0300

    add exit headder

---
+++ include/exit.h
@@ -0,0 +1,8 @@
+/* exit.h (c) Ch4r0nN/Gr3n0xX */
+#ifndef _EXIT_H_
+#define _EXIT_H_
+
+#define SYSCALL_PARAM(F_exit, E_proc)
+    __asm__("int $0x80" :: "a" (F_exit), "b" (E_proc));
+
+#endif /*_EXIT_H*/
