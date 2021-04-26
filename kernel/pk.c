36d8e2371a3c9a89411c3d7b9b055048c0dcd74a
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-04-25 21:26:30 -0300

    Add files via upload

---
+++ kernel/pk.c
@@ -0,0 +1,18 @@
+#include <stdarg.h>
+#include <stddef.h>
+
+#include "fleak/kernel.h"
+
+static char buf[1024];
+
+int pk(const char *fmt, ...)
+{
+	va_list args;
+	int i;
+
+	va_start(args, fmt);
+	i=vsprintf(buf,fmt,args);
+	va_end(args);
+	__asm__("push %%fs\n\t", "push %%ds\n\t", "pop %%fs\n\t", "pushl %0\n\t", "pushl $_buf\n\t", "pushl $0\n\t", "call write\n\t", "addl $8,%%esp\n\t", "popl %0\n\t", "pop %%fs", ::"r" (i):"ax","cx","dx");
+	return i;
+}
