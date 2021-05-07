15d9a2471c2710000795670012a6f047f58f8df3
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-05-07 18:17:58 -0300

    Upload standard IO headder

---
+++ include/stdio.h
@@ -0,0 +1,15 @@
+/* Copyright (c) 2021 Ch4r0nN/Gr3n0xX */
+
+#ifndef _STDIO_H_
+#define _STDIO_H_
+
+#define stdin 0
+#define stdout 1
+#define stderr 2
+
+int printf(const char *fmt, ...);
+
+int scanf(char *format, unsigned args);
+
+
+#endif /* _STDIO_H_ */
