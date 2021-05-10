8bb911c29a55e4a27d87f68610ad350fcb6f4b0e
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-05-10 13:14:52 -0300

    Update kernel.h

--- include/fleak/kernel.h
+++ include/fleak/kernel.h
@@ -7,7 +7,7 @@ void kernel_panic(const char * str);
 
 int printf(const char * fmt, ...);
 
-int pk(const char * fmt, ...);
+int printk(const char * fmt, ...);
 
 int write(unsigned ch,char * buf,int count);
 
