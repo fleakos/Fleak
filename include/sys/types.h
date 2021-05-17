7ac28770c11842aeb8e2b45603dbda4940c6de83
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-05-16 22:46:00 -0400

    types.h relase 1.0.0.0

---
+++ include/sys/types.h
@@ -0,0 +1,16 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#ifndef _SIZE_T
+#define _SIZE_T
+
+typedef unsigned int size_t;
+#endif /* _SIZE_T */
+
+#ifndef _NULL
+#define _NULL
+
+#define NULL ((void *)0)
+#endif /* _NULL */
+
+#endif /* _TYPES_H_ */
