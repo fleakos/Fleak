587799fb8ab3dacd2a83ed3524b452bba18f76c2
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-06-02 18:41:14 -0400

    update kernel_panic.c

--- kernel/kernel_panic.c
+++ kernel/kernel_panic.c
@@ -1,7 +1,7 @@
 #include "../include/fleak/kernel.h"
 
 volatile void kernel_panic(const char * s){
-    pk("Kernel panic: %s\n\n\r", s);
+    printk("Kernel panic: %s\n\n\r", s);
     for(;;);
 }
 
