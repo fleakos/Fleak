a1dc15ba765f19655d7b511b96e2f0872e80ef35
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-06-06 23:19:16 -0400

    correcting errors

--- include/machine/x86.h
+++ include/machine/x86.h
@@ -1,8 +1,8 @@
 #include "../include/sys/types.h"
 
 
-static inline u_char
-inb(u_short32 port)
+static inline u_char8
+inb(u_short16 port)
 {
   u_char8 data;
 
@@ -20,13 +20,13 @@ insl(int port, void *addr, int cnt)
 }
 
 static inline void
-outb(u_short32 port, u_char8 data)
+outb(u_short16 port, u_char8 data)
 {
   asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
 
 static inline void
-outw(u_short32 port, u_short32 data)
+outw(u_short16 port, u_short16 data)
 {
   asm volatile("out %0,%1" : : "a" (data), "d" (port));
 }
@@ -63,11 +63,11 @@ struct segdesc;
 static inline void
 lgdt(struct segdesc *p, int size)
 {
-  volatile u_short32 pd[3];
+  volatile u_short16 pd[3];
 
   pd[0] = size-1;
-  pd[1] = (u_int64)p;
-  pd[2] = (u_int64)p >> 16;
+  pd[1] = (u_int32)p;
+  pd[2] = (u_int32)p >> 16;
 
   asm volatile("lgdt (%0)" : : "r" (pd));
 }
@@ -77,31 +77,31 @@ struct gatedesc;
 static inline void
 lidt(struct gatedesc *p, int size)
 {
-  volatile u_short32 pd[3];
+  volatile u_short16 pd[3];
 
   pd[0] = size-1;
-  pd[1] = (u_int64)p;
-  pd[2] = (u_int64)p >> 16;
+  pd[1] = (u_int32)p;
+  pd[2] = (u_int32)p >> 16;
 
   asm volatile("lidt (%0)" : : "r" (pd));
 }
 
 static inline void
-ltr(u_short32 sel)
+ltr(u_short16 sel)
 {
   asm volatile("ltr %0" : : "r" (sel));
 }
 
-static inline u_int64
+static inline u_int32
 readeflags(void)
 {
-  u_int64 eflags;
+  u_int32 eflags;
   asm volatile("pushfl; popl %0" : "=r" (eflags));
   return eflags;
 }
 
 static inline void
-loadgs(u_short32 v)
+loadgs(u_short16 v)
 {
   asm volatile("movw %0, %%gs" : : "r" (v));
 }
@@ -118,10 +118,10 @@ sti(void)
   asm volatile("sti");
 }
 
-static inline u_int64
-xchg(volatile u_int64 *addr, u_int64 newval)
+static inline u_int32
+xchg(volatile u_int32 *addr, u_int64 newval)
 {
-  u_int64 result;
+  u_int32 result;
   
   // The + in "+m" denotes a read-modify-write operand.
   asm volatile("lock; xchgl %0, %1" :
@@ -131,49 +131,51 @@ xchg(volatile u_int64 *addr, u_int64 newval)
   return result;
 }
 
-static inline u_int64
+static inline u_int32
 rcr2(void)
 {
-  u_int64 val;
+  u_int32 val;
   asm volatile("movl %%cr2,%0" : "=r" (val));
   return val;
 }
 
 static inline void
-lcr3(u_int64 val) 
+lcr3(u_int32 val) 
 {
   asm volatile("movl %0,%%cr3" : : "r" (val));
 }struct trapframe {
   // registers as pushed by pusha
-  u_int64 edi;
-  u_int64 esi;
-  u_int64 ebp;
-  u_int64 oesp;      // useless & ignored
-  u_int64 ebx;
-  u_int64 edx;
-  u_int64 ecx;
-  u_int64 eax;
+  /* historically registers starting with "e" of extended have 32 bits */
+  
+  u_int32 edi;
+  u_int32 esi;
+  u_int32 ebp;
+  u_int32 oesp;      // useless & ignored
+  u_int32 ebx;
+  u_int32 edx;
+  u_int33 ecx;
+  u_int32 eax;
 
   // rest of trap frame
-  u_short32 gs;
-  u_short32 padding1;
-  u_short32 fs;
-  u_short32 padding2;
-  u_short32 es;
-  u_short32 padding3;
-  u_short32 ds;
-  u_short32 padding4;
-  u_int64 trapno;
+  u_short16 gs;
+  u_short16 padding1;
+  u_short16 fs;
+  u_short16 padding2;
+  u_short16 es;
+  u_short16 padding3;
+  u_short16 ds;
+  u_short16 padding4;
+  u_int32 trapno;
 
   // below here defined by x86 hardware
-  u_int64 err;
-  u_int64 eip;
-  u_short32 cs;
-  u_short32 padding5;
-  u_int64 eflags;
+  u_int32 err;
+  u_int32 eip;
+  u_short16 cs;
+  u_short16 padding5;
+  u_int32 eflags;
 
   // below here only when crossing rings, such as from user to kernel
-  u_int64 esp;
-  u_short32 ss;
-  u_short32 padding6;
+  u_int32 esp;
+  u_short16 ss;
+  u_short16 padding6;
 };
