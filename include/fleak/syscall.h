c97b8c29becb42e1e85e033f23f726b9a795996a
Ch4r0nN λ <82242552+Ch4r0nN@users.noreply.github.com>
2021-07-15 03:04:06 -0400

    update syscall.h

--- include/fleak/syscall.h
+++ include/fleak/syscall.h
@@ -4,15 +4,24 @@
 /* syscall_table */
 
 enum{
-    SYS_fork,
-    SYS_exit,
     SYS_read,
-    SYS_kill,
-    SYS_sleep,
     SYS_open,
     SYS_write,
     SYS_close,
-    SYS_time
+    SYS_fork
 };
+
+/* 
+* table of syscalls:
+*
+* SYS_call_read
+* SYS_call_open
+* SYS_call_write
+* SYS_call_close
+* SYS_call_fork
+*/
+
+void syscall_entry(void);
+void syscall(void);
 
 #endif /* _SYSCALL_H_ */
